f6280854f9818dc6e7e2dd39323ebf488610e15c
Rus8921 <Artom1223@yandex.ru>
2023-03-12 13:40:49 +0300

    1

---
+++ src/Entity/Lesson/lesson.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity\Lesson;
+use App\Entity\pack\pack;
+use Cassandra\Date;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use ApiPlatform\Metadata\ApiResource;
+
+#[ApiResource]
+#[ORM\Entity]
+class lesson
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "integer",nullable: true)]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    private ?int $id = null;
+
+    #[ORM\Column(type: "string", length: 255)]
+    #[Assert\NotBlank]
+    private ?string $title = null;
+
+    #[ORM\Column(type: "text")]
+    #[Assert\NotBlank]
+    private ?string $description = null;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private ?string $topic = null;
+
+    #[ORM\Column(type: "date")]
+    #[Assert\NotBlank]
+    private ?DateTimeInterface $date = null;
+
+    #[ORM\Column(type: "time", nullable: true)]
+    private ?DateTimeInterface $start_time = null;
+
+    #[ORM\Column(type: "time", nullable: true)]
+    private ?DateTimeInterface $end_time = null;
+
+    #[ORM\ManyToOne(inversedBy: 'lessons')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?pack $pack = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string|null $title
+     */
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTopic(): ?string
+    {
+        return $this->topic;
+    }
+
+    /**
+     * @param string|null $topic
+     */
+    public function setTopic(?string $topic): void
+    {
+        $this->topic = $topic;
+    }
+
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getDate(): ?DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param DateTimeInterface|null $date
+     */
+    public function setDate(?DateTimeInterface $date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getStartTime(): ?DateTimeInterface
+    {
+        return $this->start_time;
+    }
+
+    /**
+     * @param DateTimeInterface|null $start_time
+     */
+    public function setStartTime(?DateTimeInterface $start_time): void
+    {
+        $this->start_time = $start_time;
+    }
+
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getEndTime(): ?DateTimeInterface
+    {
+        return $this->end_time;
+    }
+
+    /**
+     * @param DateTimeInterface|null $end_time
+     */
+    public function setEndTime(?DateTimeInterface $end_time): void
+    {
+        $this->end_time = $end_time;
+    }
+
+    public function getPack(): ?pack
+    {
+        return $this->pack;
+    }
+
+    public function setPack(?pack $pack): self
+    {
+        $this->pack = $pack;
+
+        return $this;
+    }
+
+
+
+}
---
+++ src/Entity/pack/pack.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity\pack;
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\Lesson\lesson;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource]
+#[ORM\Entity]
+
+// pack means group of people or just group
+class pack
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "integer", nullable: true)]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    private ?int $id= null;
+
+    #[ORM\Column(type: "string", length: 255)]
+    #[Assert\NotBlank]
+    private ?string $packName= null;
+
+    #[ORM\Column(type: "string")]
+    #[Assert\NotBlank]
+    private ?string $calendar_url=null;
+
+    #[ORM\OneToMany(mappedBy: 'pack', targetEntity: lesson::class)]
+    private Collection $lessons;
+
+    public function __construct()
+    {
+        $this->lessons = new ArrayCollection();
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPackName(): ?string
+    {
+        return $this->packName;
+    }
+
+    /**
+     * @param string|null $packName
+     */
+    public function setPackName(?string $packName): void
+    {
+        $this->packName = $packName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCalendarUrl(): ?string
+    {
+        return $this->calendar_url;
+    }
+
+    /**
+     * @param string|null $calendar_url
+     */
+    public function setCalendarUrl(?string $calendar_url): void
+    {
+        $this->calendar_url = $calendar_url;
+    }
+
+    /**
+     * @return Collection<int, lesson>
+     */
+    public function getLessons(): Collection
+    {
+        return $this->lessons;
+    }
+
+//    public function addLesson(lesson $lesson): self
+//    {
+//        if (!$this->lessons->contains($lesson)) {
+//            $this->lessons->add($lesson);
+//            $lesson->setPack($this);
+//        }
+//
+//        return $this;
+//    }
+
+
+
+}
